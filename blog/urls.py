59ca877234b0053f5f403196a7807a56751e7f55
Fan Ng <ngwf0003@gmail.com>
2023-12-01 16:46:19 +0800

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+]
+
+
+# from . import views
+# urlpatterns = [
+#    path('', views.post_list, name='post_list'),
+# ]
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Django Girl Tutorial
+
+This is the file edited by Frankie based on Drango Girl
+
+Prepared for Django Class
